04fbec5fdb0d5da17f393f0b8d5c3b26bb14d025
Your Name <mohamed.bouazzouz@gmail.com>
2025-06-27 13:02:36 +0200

    setup sanity cms

---
+++ src/sanity/schemaTypes/index.ts
@@ -0,0 +1,9 @@
+import { type SchemaTypeDefinition } from "sanity";
+import { promotionCode } from "./schemas/promotion-code";
+import { promotionCampaign } from "./schemas/promotion-campaign";
+import { productCategory } from "./schemas/product-category";
+import { product } from "./schemas/product";
+
+export const schema: { types: SchemaTypeDefinition[] } = {
+  types: [promotionCode, promotionCampaign, productCategory, product],
+};
