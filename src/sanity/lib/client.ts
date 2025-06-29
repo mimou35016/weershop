0af0ace4f8539208f559f6a7f154236005d96a5a
Your Name <mohamed.bouazzouz@gmail.com>
2025-06-29 18:11:35 +0200

    add search feature

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -30,3 +30,14 @@ export const getProductsByCategorySlug = async (slug: string) => {
   const products = await sanityFetch({ query: query, params: { slug } });
   return products.data as Product[];
 };
+export const searchProducts = async (searchQuery: string) => {
+  const query = `*[_type == "product" && (
+    title match "*" + $searchQuery + "*" ||
+    description match "*" + $searchQuery + "*" ||
+    category->title match "*" + $searchQuery + "*" ||
+    category->slug.current match "*" + $searchQuery + "*"
+  )]`;
+
+  const products = await sanityFetch({ query: query, params: { searchQuery } });
+  return products.data as Product[];
+};
